7f70f64cbecc42e77f9ad80be6a63f530863ed27
Abdelrahman Magdy <magdyabdo484@gmail.com>
2026-06-19 18:25:04 +0300

    Add getGot method and their unitt tests

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,19 @@ public class App {
         return "Hello World!";
     }
 
+    public String getGoat(String player) {
+        if(player.equalsIgnoreCase("Ronaldo")) {
+            return "Ronaldo is the GOAT";
+        } else if (player.equalsIgnoreCase("Messi")) {
+            return "Messi is the GOAT";
+        }
+        else {
+            return "Unknown player";
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        System.out.println(new App().getGoat("Messi"));
     }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -8,4 +8,29 @@ public class AppTest {
         String greeting = app.getGreeting();
         assert greeting.equals("Hello World!") : "Expected 'Hello World!' but got '" + greeting + "'";
     }
+
+    @Test
+    public void testGetGoatWhenGoatIsRonaldo() {
+        App app = new App();
+
+        String ronaldoGoat = app.getGoat("Ronaldo");
+        assert ronaldoGoat.equals("Ronaldo is the GOAT") : "Expected 'Ronaldo is the GOAT' but got '" + ronaldoGoat + "'";
+    }
+
+    @Test
+    public void testGetGoatWhenGoatIsMessi() {
+        App app = new App();
+
+        String messiGoat = app.getGoat("Messi");
+        assert messiGoat.equals("Messi is the GOAT") : "Expected 'Messi is the GOAT' but got '" + messiGoat + "'";
+    }
+
+    @Test
+    public void testGetGoatWhenGoatIsUnknown() {
+        App app = new App();
+
+        String unknownGoat = app.getGoat("Neymar");
+        assert unknownGoat.equals("Unknown player") : "Expected 'Unknown player' but got '" + unknownGoat + "'";
+
+    }
 }
