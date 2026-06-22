62f3f5ea039c46e6624d1255ef269d792c7a4e1b
Abdelrahman Magdy <magdyabdo484@gmail.com>
2026-06-22 15:58:11 +0300

    refactor: convert standalone java applciation to spring boot app

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,36 +1,5 @@
-import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class AppTest {
-
-    @Test
-    public void testGetGreeting() {
-        App app = new App();
-        String greeting = app.getGreeting();
-        assert greeting.equals("Hello World!") : "Expected 'Hello World!' but got '" + greeting + "'";
-    }
-
-    @Test
-    public void testGetGoatWhenGoatIsRonaldo() {
-        App app = new App();
-
-        String ronaldoGoat = app.getGoat("Ronaldo");
-        assert ronaldoGoat.equals("Ronaldo is the GOAT") : "Expected 'Ronaldo is the GOAT' but got '" + ronaldoGoat + "'";
-    }
-
-    @Test
-    public void testGetGoatWhenGoatIsMessi() {
-        App app = new App();
-
-        String messiGoat = app.getGoat("Messi");
-        assert messiGoat.equals("Messi is the GOAT") : "Expected 'Messi is the GOAT' but got '" + messiGoat + "'";
-    }
-
-    @Test
-    public void testGetGoatWhenGoatIsUnknown() {
-        App app = new App();
-
-        String unknownGoat = app.getGoat("Neymar");
-        assert unknownGoat.equals("Unknown player") : "Expected 'Unknown player' but got '" + unknownGoat + "'";
-
-    }
 }
